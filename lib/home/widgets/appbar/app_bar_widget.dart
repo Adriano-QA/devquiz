5c31d43ddea6ccba378c1011f0d7c683822b41ca
Adriano <adrianoaluizi@gmail.com>
2021-04-19 16:35:39 -0300

    commit

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,47 @@
+import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 161,
+                    width: double.maxFinite,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                  text: "VS Lab",
+                                  style: AppTextStyles.titleBold)
+                            ])),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://scontent.fqsc2-1.fna.fbcdn.net/v/t1.6435-9/171303111_775692023117051_8081533966317036684_n.png?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=IbFETX29NY4AX-4_WEF&_nc_ht=scontent.fqsc2-1.fna&oh=4d304e1f0b5bad073d59ea3175266b47&oe=60A39232"))),
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
+                ],
+              ),
+            ));
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:DevQuiz/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DevQuiz',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
